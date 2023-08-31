137284c590ab251dc76e11fccc5b47e9a9e41d69
sajjadmolliek <sajjadmolliek2018@gmail.com>
2023-09-01 04:21:33 +0600

    add time

--- script.js
+++ script.js
@@ -47,32 +47,61 @@ callIdData = async(id) => {
         `;
         noData.appendChild(div);
     }
-    else{
+    else if(idData!=0 ){
         idData.forEach(element2 => {
-            const div = document.createElement('div');
-            div.classList = "flex justify-center mb-5";
+            let hr = parseInt((element2?.others?.posted_date)/3600);
+            let resHR = (element2?.others?.posted_date)%3600;
+            let min = parseInt(resHR/60);
+            // console.log("Houre:",hr,"Munite:",min);
+            console.log(element2?.others);
+            if(element2?.others?.posted_date!=0){
+                const div = document.createElement('div');
+                div.classList = "flex justify-center mb-5";
                 div.innerHTML = `
-            <div class="card  bg-base-100 ">
-                <figure><img class="w-[20rem] h-[12rem] rounded-lg" src="${element2?.thumbnail}" alt="thumbnail Image"></figure>
-                <div class="card-body px-4 ">
-                    <div class="flex ">
-                        <img class="w-[2rem] h-[2rem] rounded-2xl mr-4" src="${element2?.authors[0]?.profile_picture}" alt="thumbnail Image">
-                        <h1 class="text-2xl font-bold">${element2.title}</h1>
+                <div class="card  bg-base-100 ">
+                    <figure class=" flex-col ">
+                        <img class="w-[20rem] h-[12rem] rounded-lg relative" src="${element2?.thumbnail}" alt="thumbnail Image">
+                       <p class="bg-black text-white absolute bottom-[47%] right-3 px-3 rounded">${hr} hrs ${min}min ago</p>
+                    </figure>
+                    <div class="card-body px-4 ">
+                        <div class="flex ">
+                            <img class="w-[2rem] h-[2rem] rounded-2xl mr-4" src="${element2?.authors[0]?.profile_picture}" alt="thumbnail Image">
+                            <h1 class="text-2xl font-bold">${element2.title}</h1>
+                        </div>
+                        <div class=" ml-12  opacity-70">
+                            <span>${element2?.authors[0]?.profile_name}</span>
+                            <span>${element2?.authors[0]?.verified?"<img class='w-[10%] inline' src='./image/ok.png' alt='Verified'>":""}</span>
+                        </div>
+                        <p class="ml-[3rem] opacity-60">${element2?.others?.views} views</p>
                     </div>
-                    <div class=" ml-12  opacity-70">
-                        <span>${element2?.authors[0]?.profile_name}</span>
-                        <span>${element2?.authors[0]?.verified?"<img class='w-[10%] inline' src='./image/ok.png' alt='Verified'>":""}</span>
-                    </div>
-                    <p class="ml-[3rem] opacity-60">${element2?.others?.views} views</p>
                 </div>
-            </div>
             `
             playListSection.appendChild(div);
-            // let hr = parseInt((element2?.others?.posted_date)/3600);
-            // let resHR = (element2?.others?.posted_date)%3600;
-            // let min = parseInt(resHR/3600);
-            // console.log("Houre:",hr,"Munite:",min);
-            console.log(element2?.others?.posted_date);
+            }
+            else{
+                const div = document.createElement('div');
+                div.classList = "flex justify-center mb-5";
+                div.innerHTML = `
+                <div class="card  bg-base-100 ">
+                    <figure>
+                        <img class="w-[20rem] h-[12rem] rounded-lg" src="${element2?.thumbnail}" alt="thumbnail Image">
+                    </figure>
+                    <div class="card-body px-4 ">
+                        <div class="flex ">
+                            <img class="w-[2rem] h-[2rem] rounded-2xl mr-4" src="${element2?.authors[0]?.profile_picture}" alt="thumbnail Image">
+                            <h1 class="text-2xl font-bold">${element2.title}</h1>
+                        </div>
+                        <div class=" ml-12  opacity-70">
+                            <span>${element2?.authors[0]?.profile_name}</span>
+                            <span>${element2?.authors[0]?.verified?"<img class='w-[10%] inline' src='./image/ok.png' alt='Verified'>":""}</span>
+                        </div>
+                        <p class="ml-[3rem] opacity-60">${element2?.others?.views} views</p>
+                    </div>
+                </div>
+            `
+            playListSection.appendChild(div);
+            }
+            
         });
     }
 }
